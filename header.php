d37df3638bc86b5ec4f360c6b4501558c37d0858
Charlie Thomason <charlie.thomason@razorfish.com>
2015-01-09 15:29:51 -0600

    Moved main nav into header

--- header.php
+++ header.php
@@ -16,35 +16,32 @@
 			<a href="<?php echo get_option('home'); ?>"><span class="first-name">Charlie</span> <span class="last-name">Thomason</span></a>
 		</h1>
 
-		<aside id="social">
+
+		<nav id="main-nav">
 			<ul>
-				<li><a href="http://twitter.com/charliethomason" alt="Twitter" rel="nofollow" target="_blank" title="Twitter"><img src="<?php bloginfo('template_directory'); ?>/images/twitter02.png" alt="Twitter"></a></li>
-				<li><a href="http://instagram.com/charliethomason" alt="Instagram" rel="nofollow" target="_blank" title="Instagram"><img src="<?php bloginfo('template_directory'); ?>/images/instagram02.png" alt="Instagram"></a></li>
+				<li<?php if (is_front_page()) { ?> class="active"<?php } ?>>
+					<a href="<?php echo get_option('home'); ?>">Home</a>
+				</li>
+				<li<?php if (is_page_template('about.php')) { ?> class="active"<?php } ?>>
+					<a href="/about">About</a>
+				</li>
+				<li<?php if (is_page_template('art.php') || is_singular('art') || is_tag()) { ?> class="active"<?php } ?>>
+					<a href="/gallery">Art</a>
+				</li>
+				<li<?php if (is_home() || is_singular('post') || is_category()) { ?> class="active"<?php } ?>>
+					<a href="/blog">Blog</a>
+				</li>
+				<li<?php if (is_page_template('shop.php')) { ?> class="active"<?php } ?>>
+					<a href="/shop">Shop</a>
+				</li>
+				<li class="social-icon"><a href="http://twitter.com/charliethomason" alt="Twitter" rel="nofollow" target="_blank" title="Twitter"><img src="<?php bloginfo('template_directory'); ?>/images/twitter02.png" alt="Twitter"></a></li>
+				<li class="social-icon"><a href="http://instagram.com/charliethomason" alt="Instagram" rel="nofollow" target="_blank" title="Instagram"><img src="<?php bloginfo('template_directory'); ?>/images/instagram02.png" alt="Instagram"></a></li>
 			</ul>
-		</aside>
+		</nav>
+
 	</div>
 </header>
 
 <main id="main">
-
-	<nav id="main-nav">
-		<ul>
-			<li<?php if (is_front_page()) { ?> class="active"<?php } ?>>
-				<a href="<?php echo get_option('home'); ?>">Home</a>
-			</li>
-			<li<?php if (is_page_template('about.php')) { ?> class="active"<?php } ?>>
-				<a href="/about">About</a>
-			</li>
-			<li<?php if (is_page_template('art.php') || is_singular('art') || is_tag()) { ?> class="active"<?php } ?>>
-				<a href="/gallery">Art</a>
-			</li>
-			<li<?php if (is_home() || is_singular('post') || is_category()) { ?> class="active"<?php } ?>>
-				<a href="/blog">Blog</a>
-			</li>
-			<li<?php if (is_page_template('shop.php')) { ?> class="active"<?php } ?>>
-				<a href="/shop">Shop</a>
-			</li>
-		</ul>
-	</nav>
 
 	<section id="content">
